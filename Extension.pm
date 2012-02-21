5a79f31e073bcfabf8a7cfbca447ec2660dbba43
Scott Saad <saadsj@gmail.com>
2012-02-20 23:07:27 -0700

    Improved the notifications when specific bug fields changed. Status, Priority, Severity, Target Milestone all report their previous and new values

--- Extension.pm
+++ Extension.pm
@@ -9,6 +9,7 @@ package Bugzilla::Extension::NotifyGrove;
 use strict;
 use base qw(Bugzilla::Extension);
 
+use Bugzilla::Field;
 use Bugzilla::User;
 use Bugzilla::Extension::NotifyGrove::Util;
 
@@ -18,40 +19,52 @@ sub bug_end_of_create {
 	my ($self, $args) = @_;
 
 	my $bug = $args->{'bug'};
-
-	my $url = sprintf( '%s/show_bug.cgi?id=%d', Bugzilla->params->{ 'urlbase' }, $bug->id  );
-	my $user = Bugzilla->user;
-	my $msg = sprintf( 'New %s :: %s bug %d filed by %s.', $bug->product, $bug->component, $bug->id, $user->name );
+	my $msg = sprintf( 'New %s :: %s bug %d filed by %s.', 
+		$bug->product, 
+		$bug->component, 
+		$bug->id, 
+		Bugzilla->user->name );
 	post_grove_msg( $msg );
 }
 
 sub bug_end_of_update {
 	my ($self, $args) = @_;
 
-	my ($bug, $old_bug, $timestamp, $changes) = 
-	@$args{qw(bug old_bug timestamp changes)};
+	my ($bug, $old_bug, $timestamp, $changes) = @$args{qw(bug old_bug timestamp changes)};
 
-	foreach my $field (keys %$changes) {
-		my $used_to_be = $changes->{$field}->[0];
-		my $now_it_is  = $changes->{$field}->[1];
+	# report specific field field changes
+	foreach my $field_name (keys %$changes) {
+		my $changed_field = new Bugzilla::Field({name => $field_name});
+		# only look for certain fields
+		if ( $changed_field->name eq 'bug_status' 
+			or $changed_field->name eq 'priority' 
+			or $changed_field->name eq 'bug_severity' 
+			or $changed_field->name eq 'target_milestone' )
+		{
+			my $used_to_be = $changes->{$field_name}->[0];
+			my $now_it_is  = $changes->{$field_name}->[1];
+			my $msg = sprintf( '%s changed %s on bug %d from %s to %s.', 
+				Bugzilla->user->name, 
+				$changed_field->description, 
+				$bug->id, 
+				$used_to_be, 
+				$now_it_is ); 
+			post_grove_msg( $msg );
+		}
 	}
 
-	my $old_summary = $old_bug->short_desc;
-	my $status_message = 'updated';
-	if (my $status_change = $changes->{'bug_status'}) {
-		my $old_status = new Bugzilla::Status({ name => $status_change->[0] });
-		my $new_status = new Bugzilla::Status({ name => $status_change->[1] });
-		if ($new_status->is_open && !$old_status->is_open) {
-			$status_message = "re-opened";
-		}
-		if (!$new_status->is_open && $old_status->is_open) {
-			$status_message = "closed";
-		}
-	}
-
+	# report  bug changes
+	my $bug_url = sprintf( '%s/show_bug.cgi?id=%d', Bugzilla->params->{ 'urlbase' }, $bug->id  );
 	my $qa_contact = defined($bug->qa_contact) ? $bug->qa_contact->name : '';
-	my $url = sprintf( '%s/show_bug.cgi?id=%d', Bugzilla->params->{ 'urlbase' }, $bug->id  );
-	my $msg = sprintf( 'Bug %s %s, %s, %s, %s, %s, %s', $url, $qa_contact, $bug->bug_severity, $bug->priority, $bug->version, $bug->bug_status, $bug->short_desc );
+	my $msg = sprintf( 'Bug %s %s, %s, %s, %s, %s, %s, %s', 
+		$bug_url, 
+		$bug->bug_status, 
+		$bug->target_milestone, 
+		$qa_contact, 
+		$bug->bug_severity, 
+		$bug->priority, 
+		$bug->version, 
+		$bug->short_desc );
 	post_grove_msg( $msg );
 }
 
